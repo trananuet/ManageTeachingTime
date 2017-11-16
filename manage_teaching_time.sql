5ece8fd3fa6a3268f4c2b9ff33cc713a03792cb5
trananuet <tranvananuet@gmail.comm>
2017-11-16 20:29:58 +0700

    update database

--- manage_teaching_time.sql
+++ manage_teaching_time.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2017 at 04:03 PM
+-- Generation Time: Nov 16, 2017 at 02:26 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -40,7 +40,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2017_11_05_152803_create_school_years_table', 1),
 (2, '2017_11_10_111245_create_semesters_table', 1),
 (3, '2014_10_12_000000_create_users_table', 2),
-(4, '2014_10_12_100000_create_password_resets_table', 2);
+(4, '2014_10_12_100000_create_password_resets_table', 2),
+(5, '2017_11_15_042608_create_user_roles_table', 3),
+(6, '2017_11_15_043140_create_roles_table', 3);
 
 -- --------------------------------------------------------
 
@@ -53,6 +55,28 @@ CREATE TABLE `password_resets` (
   `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `role` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role`, `created_at`, `updated_at`) VALUES
+(1, 'Khách', NULL, NULL),
+(2, 'Phòng đào tạo', NULL, NULL),
+(3, 'Khách', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -73,9 +97,21 @@ CREATE TABLE `school_years` (
 --
 
 INSERT INTO `school_years` (`yearID`, `name`, `active`, `created_at`, `updated_at`) VALUES
-(1, 'Năm học 2014-2015', 1, '2017-11-10 04:21:19', '2017-11-10 04:21:19'),
-(2, 'Năm học 2015-2016', 1, NULL, NULL),
-(4, 'Năm học 2017 - 2018', 1, '2017-11-10 07:53:44', '2017-11-10 07:53:44');
+(4, 'Năm học 2012 - 2013', 0, '2017-11-10 07:53:44', '2017-11-15 00:16:52'),
+(5, 'Năm học 2017 - 2018', 1, '2017-11-11 02:26:59', '2017-11-11 02:26:59'),
+(6, 'Năm học 2018 - 2019', 1, '2017-11-12 20:47:29', '2017-11-12 20:48:04'),
+(7, 'Năm học 2019- 2020', 1, '2017-11-12 20:48:21', '2017-11-12 20:48:21'),
+(12, 'sqws', 0, '2017-11-14 02:24:56', '2017-11-14 02:24:56'),
+(13, '2018', 1, NULL, NULL),
+(14, '2021', 0, NULL, NULL),
+(15, '2011', 1, NULL, NULL),
+(16, 'Năm học  2', 1, NULL, NULL),
+(17, '2021', 0, NULL, NULL),
+(18, '2011', 1, NULL, NULL),
+(19, 'Năm học  2', 1, NULL, NULL),
+(20, '2021', 0, NULL, NULL),
+(21, '2011', 1, NULL, NULL),
+(22, 'ewa', 1, '2017-11-15 00:51:21', '2017-11-15 00:51:21');
 
 -- --------------------------------------------------------
 
@@ -96,10 +132,11 @@ CREATE TABLE `semesters` (
 --
 
 INSERT INTO `semesters` (`semesterID`, `name`, `yearID`, `created_at`, `updated_at`) VALUES
-(1, 'Học kì 1', 1, NULL, NULL),
-(2, 'Học kì 2', 1, NULL, NULL),
-(3, 'Học kì 1 phụ', 1, NULL, NULL),
-(4, 'Học kì 1', 2, NULL, NULL);
+(1, 'Học kì 1', 4, NULL, '2017-11-14 22:46:31'),
+(2, 'Học kì 2', 5, NULL, '2017-11-14 22:46:35'),
+(3, 'Học kì 1 phụ', 6, NULL, '2017-11-14 22:46:39'),
+(4, 'Học kì 1', 7, NULL, '2017-11-14 22:46:45'),
+(14, '123', 4, '2017-11-12 20:34:41', '2017-11-12 20:34:41');
 
 -- --------------------------------------------------------
 
@@ -122,7 +159,31 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Trần Văn An', 'tranvananuet@gmail.com', '$2y$10$LgEdhHnNSSDcYm7ou6AJ8uEcu6YEK1sw3nSQV9B4jgWrea2Q7a40q', NULL, NULL, NULL);
+(1, 'Trần Văn An', 'tranvananuet@gmail.com', '$2y$10$LgEdhHnNSSDcYm7ou6AJ8uEcu6YEK1sw3nSQV9B4jgWrea2Q7a40q', NULL, NULL, NULL),
+(2, 'B', 'herolhp96@gmail.com', '$2y$10$S2fLYS.bmAmWFI4QfEhyZ./0U3Tet4TZan0UHp6xIWYmz5Sn6IEfy', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`id`, `user_id`, `role_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 2, NULL, NULL),
+(2, 1, 3, NULL, NULL),
+(3, 2, 1, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -139,6 +200,12 @@ ALTER TABLE `migrations`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `school_years`
@@ -160,6 +227,12 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -167,22 +240,32 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `school_years`
 --
 ALTER TABLE `school_years`
-  MODIFY `yearID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `yearID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT for table `semesters`
 --
 ALTER TABLE `semesters`
-  MODIFY `semesterID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `semesterID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
