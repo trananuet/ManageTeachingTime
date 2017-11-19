0ff47415600fbb031916e88da496519d46a45c4b
trananuet <tranvananuet@gmail.comm>
2017-11-19 18:33:33 +0700

    fix loi route o module user

--- modules/User/Http/Controllers/ManageUsers/ManageUsersController.php
+++ modules/User/Http/Controllers/ManageUsers/ManageUsersController.php
@@ -4,69 +4,16 @@ namespace Modules\User\Http\Controllers\ManageUsers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Routing\Controller;
+use App\Http\Controllers\Controller;
 
 class ManageUsersController extends Controller
 {
     /**
      * Display a listing of the resource.
-     * @return Response
+     * @return view
      */
     public function index()
     {
-        return view('user::ManageUsers.manageUsers');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     * @return Response
-     */
-    public function create()
-    {
-        return view('user::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-    }
-
-    /**
-     * Show the specified resource.
-     * @return Response
-     */
-    public function show()
-    {
-        return view('user::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @return Response
-     */
-    public function edit()
-    {
-        return view('user::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
-     */
-    public function update(Request $request)
-    {
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @return Response
-     */
-    public function destroy()
-    {
+        return view('user::manage_users.manageUsers');
     }
 }
