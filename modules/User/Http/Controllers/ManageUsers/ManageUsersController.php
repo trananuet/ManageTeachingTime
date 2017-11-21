5a018178b77db4e39eed7f01c6f6ce32418dc6df
hyperyuiz <hyperyuiz@gmail.com>
2017-11-22 02:03:07 +0700

    them quan li nguoi dung

--- modules/User/Http/Controllers/ManageUsers/ManageUsersController.php
+++ modules/User/Http/Controllers/ManageUsers/ManageUsersController.php
@@ -5,6 +5,12 @@ namespace Modules\User\Http\Controllers\ManageUsers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
+use Modules\Base\Entities\Users;
+use Modules\User\Entities\User_Roles;
+use Modules\User\Entities\Roles;
+use Modules\User\Repositories\UserRepository;
+use DB;
+
 
 class ManageUsersController extends Controller
 {
@@ -12,8 +18,14 @@ class ManageUsersController extends Controller
      * get all users
      * @return view
      */
-    public function getAllUser()
+    public function getUser()
     {
-        return view('user::manage_users.manageUsers');
+    	$users = DB::table('Users')->get();
+    	$cust=DB::table('User_Roles')
+           ->join('Users', 'User_Roles.user_id', '=', 'Users.id')
+           ->join('Roles', 'User_Roles.role_id', '=', 'Roles.id')
+           ->select('Users.name','Users.email', 'Roles.role','Users.id')
+            ->get();
+        return view('user::manage_users.manageUsers',compact('users','cust'));
     }
 }
